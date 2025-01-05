29a08c42f4163da505573288a2ea8d0a4eff91c3
Raquel Gonzalez <gonzalez.c.raquel@gmail.com>
2025-01-04 18:42:32 -0600

    exercises added

--- listas/main.py
+++ listas/main.py
@@ -1,75 +1,23 @@
-# Listas
 
-""" lista = ['string', 10, 15.6, True]
-
-print(lista) """
-
-""" 
-lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
-
-primer_curso = lista_cursos[0]
-
-segundo_curso = lista_cursos[1]
-
-ultimo_curso = lista_cursos[4]
-
-
-print(primer_curso)
-print(segundo_curso)
-print(ultimo_curso)
-
-primer_curso = lista_cursos[-5]
-
-segundo_curso = lista_cursos[-4]
-
-ultimo_curso = lista_cursos[-1]
-
-print(primer_curso)
-print(segundo_curso)
-print(ultimo_curso)
-
-
-print(lista_cursos[100])
- """
-
-""" # Actualizar
-lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
-
-lista_cursos[4] = 'Rust'
-
-print(lista_cursos) """
-
-
-# Sublistas
-# [start:end]
-# [start:end:skip]
+'''
+#Modificar listas 
 
 lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java', 'Rust']
 
-sub_lista = lista_cursos[0:3]
+
+
+lista_cursos.append('MongoDB')
+
+lista_cursos.insert(1,'Rails')
+lista_cursos.insert(0,'PyGame')
+
+
 
 print(lista_cursos)
-"""print(sub_lista)
+print(len(lista_cursos))
 
-sub_lista = lista_cursos[2:30]
-print(sub_lista) """
+'''
 
-""" sub_lista = lista_cursos[2:]
-print(sub_lista)
+# metodos de listas
 
-sub_lista = lista_cursos[:4]
-print(sub_lista) """
-
-""" 
-sub_lista = lista_cursos[1:5:2]
-print(sub_lista) """
-
-""" sub_lista = lista_cursos[:]
-print(sub_lista) """
-
-""" sub_lista = lista_cursos[::2]
-print(sub_lista) """
-
-
-sub_lista = lista_cursos[::-1]
-print(sub_lista)
+lista = [8, 90, 1, 5, 44, 132, 600, 3, 4]
---
+++ listas/matrices.py
@@ -0,0 +1,12 @@
+
+columna_a = [10, 20]
+columna_b = [30, 40]
+
+
+matriz = [columna_a, columna_b] #2x2
+
+print(matriz)
+print(matriz[0][0])
+print(matriz[0][1])
+print(matriz[1][0])
+print(matriz[1][1])
